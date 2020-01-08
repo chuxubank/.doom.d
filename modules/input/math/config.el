1848944ae98cff61c7895cc2800966158bce2694
Asuna <chuxubank@qq.com>
2020-01-09 00:18:21 +0800

    add math module

---
+++ modules/input/math/config.el
@@ -0,0 +1,17 @@
+;;; input/math/config.el -*- lexical-binding: t; -*-
+
+;;;###package cdlatex
+(add-hook! '(org-mode-hook LaTeX-mode-hook) #'turn-on-org-cdlatex)
+(setq cdlatex-env-alist
+      '(("equation*" "\\begin{equation*}\n?\n\\end{equation*}" nil)
+        ("cases" "\\begin{cases}\n? & ,\\\\\n & .\n\\end{cases}" nil)
+        ("tikzpicture" "\\begin{tikzpicture}\n?\n\\end{tikzpicture}" nil)))
+(setq cdlatex-command-alist
+      '(("equ*" "Insert equation* env" "" cdlatex-environment ("equation*") t nil)
+        ("cas" "Insert cases env" "" cdlatex-environment ("cases") t nil)
+        ("tikz" "Insert tikzpicture env" "" cdlatex-environment ("tikzpicture") t nil)))
+(setq cdlatex-math-modify-alist
+      '((86 "\\vb*" nil t nil nil)
+        (98 "\\bm" nil t nil nil)))
+(setq cdlatex-math-symbol-alist
+      '((76 ("\\Lambda" "\\varLambda"))))
---
+++ modules/input/math/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; input/math/packages.el
+
+(package! cdlatex)
