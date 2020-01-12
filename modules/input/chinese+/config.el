199dc7e5cb65245b9ec90133b7d04720aa1e8341
Asuna <chuxubank@qq.com>
2020-01-12 16:59:35 +0800

    move xelatex related config to chinese+ module

--- modules/input/chinese+/config.el
+++ modules/input/chinese+/config.el
@@ -20,10 +20,20 @@
   (setq pyim-default-scheme 'rime-quanpin))
 
 ;;;###package org
-(setq org-latex-compiler "xelatex")
+(setq org-latex-compiler "xelatex"
+      org-preview-latex-default-process 'xdvsvg)
 (after! org
-  (add-to-list 'org-latex-packages-alist
-               '("" "ctex" t ("xelatex"))))
+  (add-to-list 'org-latex-packages-alist '("" "ctex" t ("xelatex")))
+  (add-to-list 'org-preview-latex-process-alist '(xdvsvg :programs ("xelatex" "dvisvgm")
+                                                         :description "xdv > svg"
+                                                         :message "you need to install the programs: xelatex and dvisvgm."
+                                                         :use-xcolor t
+                                                         :image-input-type "xdv"
+                                                         :image-output-type "svg"
+                                                         :image-size-adjust (1.7 . 1.5)
+                                                         :latex-compiler ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
+                                                         :image-converter ("dvisvgm %f -n -b min -c %S -o %O"))))
+
 
 (map!
  "s-i" #'pyim-convert-string-at-point
