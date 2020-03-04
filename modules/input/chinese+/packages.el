0fb1ad98f084220630cbe9500b6eedad8a4b0039
Asuna <chuxubank@qq.com>
2020-03-04 11:35:30 +0800

    Bump liberime

--- modules/input/chinese+/packages.el
+++ modules/input/chinese+/packages.el
@@ -4,4 +4,4 @@
 (package! posframe)
 (package! liberime
   :recipe (:host github :repo "merrickluo/liberime" :files ("CMakeLists.txt" "Makefile" "src" "liberime-config.el"))
-  :pin "ec4af812454")
+  :pin "6080de13f5")
