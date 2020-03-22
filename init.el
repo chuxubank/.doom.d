95055629b0e6694a43a54467cff04cc2ab930cc5
Asuna <chuxubank@qq.com>
2020-03-22 11:01:37 +0800

    Add gist module

--- init.el
+++ init.el
@@ -87,7 +87,7 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
+       gist              ; interacting with github gists
        (lookup +docsets +dictionary)              ; navigate your code and its documentation
        lsp
        macos             ; MacOS-specific commands
