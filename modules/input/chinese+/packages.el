65b5c1850bccc1db6408282510e04502e4cbdda3
Asuna <chuxubank@qq.com>
2020-05-17 10:10:54 +0800

    Bump chinese+ packages

--- modules/input/chinese+/packages.el
+++ modules/input/chinese+/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/chinese+/packages.el
 
-(package! posframe :pin "83b4d39fdd")
-(package! rime :pin "d1d9008f9d")
-(package! pangu-spacing :pin "f92898949b")
+(package! posframe :pin "093b29a53cbeda6d637ccc9ef4dfc47123e79b9e")
+(package! rime :pin "daf956df30d4007f70a4495560d656e517ab69a9")
+(package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
