3ee2bbbe49eaf612b26cc1deffba12affcbce2b7
Asuna <chuxubank@qq.com>
2020-05-21 22:07:51 +0800

    Fix pdf-tools annot list key binding
    Use emacs mode as default

--- config.el
+++ config.el
@@ -53,8 +53,16 @@
 
 ;;;###package evil
 (setq! evil-disable-insert-state-bindings t)
-(evil-set-initial-state 'osx-dictionary-mode 'emacs)
+(set-evil-initial-state!
+  '(osx-dictionary-mode
+    pdf-tools-modes
+    pdf-annot-list-mode)
+  'emacs)
 
+;;;###package evil-collection
+(appendq! +evil-collection-disabled-list '(pdf))
+
+;;;###package format-all
 (appendq! +format-on-save-enabled-modes '(nxml-mode))
 
 (map! :leader
