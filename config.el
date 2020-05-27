9808c9744fcbc8d7eac9b674c03e027b7d68f863
Asuna <chuxubank@qq.com>
2020-05-28 07:39:54 +0800

    Add pitch font for treemacs display

--- config.el
+++ config.el
@@ -22,6 +22,7 @@
     (setq doom-font (font-spec :family "Sarasa Mono SC")
           doom-unicode-font (font-spec :family "Sarasa Mono SC"))
   (setq doom-font (font-spec :family "Sarasa Mono SC" :size 16)))
+(setq doom-variable-pitch-font (font-spec :family "Sarasa Mono SC"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
