a3deb3a8f6f21eaed6926abac55f03b640f9302a
Misaka <chuxubank@qq.com>
2020-05-30 12:55:57 +0800

    Bump module :input chinese+

--- modules/input/chinese+/packages.el
+++ modules/input/chinese+/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/chinese+/packages.el
 
-(package! posframe :pin "093b29a53cbeda6d637ccc9ef4dfc47123e79b9e")
-(package! rime :pin "daf956df30d4007f70a4495560d656e517ab69a9")
+(package! posframe :pin "6285217711bc846e565940261829b523e298f82e")
+(package! rime :pin "ab527f062d747b3376b53b5d350bff0a29f80087")
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
