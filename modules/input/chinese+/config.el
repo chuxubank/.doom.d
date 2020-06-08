942ace4250cefee993c1f094728144e12d6f273f
Asuna <chuxubank@qq.com>
2020-06-08 08:38:21 +0800

    Add rime predicates

--- modules/input/chinese+/config.el
+++ modules/input/chinese+/config.el
@@ -8,10 +8,13 @@
   (rime-posframe-style 'vertical)
   (rime-cursor "˰")
   (rime-disable-predicates '(rime-predicate-evil-mode-p
-                             rime-predicate-after-ascii-char-p
                              rime-predicate-prog-in-code-p
                              rime-predicate-org-latex-mode-p
-                             rime-predicate-org-in-src-block-p)))
+                             rime-predicate-org-in-src-block-p
+                             rime-predicate-after-ascii-char-p
+                             rime-predicate-tex-math-or-command-p
+                             rime-predicate-punctuation-line-begin-p
+                             rime-predicate-punctuation-after-space-cc-p)))
 (when IS-MAC
   (setq! rime-librime-root (expand-file-name "librime/dist" doom-local-dir)))
 
