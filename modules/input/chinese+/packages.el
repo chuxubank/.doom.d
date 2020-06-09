38d8763edec9f241b8ecf04cc9e2841e8d1df67f
Jerry.Chu <jchu@i-zhiyan.com>
2020-06-09 15:23:06 +0800

    Bump chinese+ module

--- modules/input/chinese+/packages.el
+++ modules/input/chinese+/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/chinese+/packages.el
 
-(package! posframe :pin "6285217711bc846e565940261829b523e298f82e")
-(package! rime :pin "02b91af00920bc8af93f49f6dc923ed6c05f730e")
+(package! posframe :pin "c4459028fbe6740315ff1ed6f37e8c4decd3d050")
+(package! rime :pin "2a0b116d56bf54456eb5d6e8e80a7a6cf6944863")
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
