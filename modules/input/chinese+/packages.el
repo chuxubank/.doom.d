83036f3e6dcfb120335fae3f8ee0c24117c6758c
Asuna <chuxubank@qq.com>
2020-06-30 07:38:26 +0800

    Bump private modules

--- modules/input/chinese+/packages.el
+++ modules/input/chinese+/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/chinese+/packages.el
 
-(package! posframe :pin "c4459028fbe6740315ff1ed6f37e8c4decd3d050")
-(package! rime :pin "2a0b116d56bf54456eb5d6e8e80a7a6cf6944863")
+(package! posframe :pin "922e4d239f7a083213d856de67a9686a091b1e27")
+(package! rime :pin "353a0bfee1bfe7f53d7e5cb9e9abedce60ca12a1")
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
