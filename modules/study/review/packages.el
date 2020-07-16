65447b4a7dfb4bb0dc69bb39e634db97404047a0
Asuna <chuxubank@qq.com>
2020-07-16 19:58:11 +0800

    Unpin org-roam-server

--- modules/study/review/packages.el
+++ modules/study/review/packages.el
@@ -2,7 +2,7 @@
 ;;; study/review/packages.el
 
 (package! org-drill :pin "35c1ce349949cc213f3076799211210f49431850")
-(package! org-roam-server :pin "7617ac01a1b7bad61407916ef03eda95c17da7c9")
+(package! org-roam-server)
 (package! anki-editor :pin "546774a453ef4617b1bcb0d1626e415c67cc88df")
 (package! bing-dict :pin "1d581aaa9622b34f8fb83af5579fa252aa24cfef")
 (package! org-fc :recipe (:host github :repo "l3kn/org-fc" :files (:defaults "awk" "demo.org")))
