bf47db069b1839c5241aeb1af0634da7aca615f7
Asuna <chuxubank@qq.com>
2020-08-23 00:15:04 +0800

    Add default config for anki vocabulary

--- modules/study/review/config.el
+++ modules/study/review/config.el
@@ -18,7 +18,13 @@
 (use-package! anki-vocabulary
   :defer t
   :bind (:map doom-leader-map
-         ("d" . #'anki-vocabulary)))
+         ("d" . #'anki-vocabulary))
+  :custom
+  (anki-vocabulary-deck-name "Inbox")
+  (anki-vocabulary-model-name "AutoCopy")
+  (anki-vocabulary-field-alist '(("Front" . "${expression:单词}")
+                                 ("Back" . "${sentence_bold:标粗的原文例句}")))
+  )
 
 ;;;###package org-roam
 (setq! org-roam-db-location (expand-file-name "org-roam.db" doom-cache-dir))
