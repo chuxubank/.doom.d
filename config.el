09e9b1e9a58ba351e3da9a01301189431f148a6f
Misaka <chuxubank@qq.com>
2020-09-02 17:12:40 +0800

    Change font on wsl

--- config.el
+++ config.el
@@ -17,9 +17,14 @@
 ;;
 ;;; UI
 
-(if IS-WINDOWS
-    (setq doom-font (font-spec :family "Fira Code")
-          doom-unicode-font (font-spec :family "DengXian"))
+(when IS-WINDOWS
+  (setq doom-font (font-spec :family "Fira Code")
+        doom-unicode-font (font-spec :family "DengXian")))
+
+(when IS-LINUX
+  (setq doom-font (font-spec :family "Fira Code")))
+
+(when IS-MAC
   (setq doom-font (font-spec :family "Fira Code" :size 16)))
 
 ;; maximized at startup
