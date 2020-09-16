bed48d54040358edb2099cdd00101395faf045a2
Asuna <chuxubank@qq.com>
2020-09-16 22:57:26 +0800

    Add auto-image-file-mode to custom config

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-image-file-mode t)
  '(org-format-latex-header
    "\\documentclass[dvisvgm]{article}
 \\usepackage[usenames]{color}
