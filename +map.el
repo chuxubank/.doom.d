4ba3c73853f34bdef317b533276913611cbc6c88
Asuna <chuxubank@qq.com>
2020-09-26 23:01:25 +0800

    Add map settings

--- +map.el
+++ +map.el
@@ -8,15 +8,29 @@
   'emacs)
 
 (map! :i "C-SPC" nil
+      (:after ivy :map ivy-minibuffer-map
+       "C-SPC" nil)
+      (:after counsel :map counsel-ag-map
+       "C-SPC" nil)
       (:after evil-org :map evil-org-mode-map
        :i "C-l" nil
        :i "C-h" nil
        :i "C-k" nil
        :i "C-j" nil))
 
-(map! (:map help-map
+(map! (:leader
+       :prefix-map ("a" . "app")
+       (:when (featurep! :app eaf)
+        :desc "EAF"             "e" #'eaf)
+       (:when (featurep! :app telega)
+        :desc "Telega"          "t" #'telega)
+       (:when (featurep! :app leetcode)
+        :desc "LeetCode"        "l" #'leetcode))
+
+      (:map help-map
        "dU" #'doom/upgrade)
+
       (:when (featurep! :input chinese+)
        (:map rime-mode-map
         "C-`" #'rime-send-keybinding
-        "s-i" #'rime-force-enable)))
+        "C-i" #'rime-force-enable)))
--- modules/app/telega/config.el
+++ modules/app/telega/config.el
@@ -2,7 +2,5 @@
 
 (use-package! telega
   :commands (telega)
-  :bind (:map doom-leader-map
-         ("a t" . #'telega))
   :config
   (add-hook 'telega-load-hook 'telega-mode-line-mode))
