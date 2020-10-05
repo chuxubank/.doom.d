d223ab6152d2a94e0bb81019a1731ee29b9d4418
Asuna <chuxubank@qq.com>
2020-10-05 22:22:05 +0800

    Add pdf+ module

---
+++ modules/study/pdf+/config.el
@@ -0,0 +1,8 @@
+;;; study/pdf+/config.el -*- lexical-binding: t; -*-
+
+(use-package! org-noter-pdftools
+  :when (featurep! :study pdf+ +noter)
+  :after org-noter
+  :config
+  (with-eval-after-load 'pdf-annot
+    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
