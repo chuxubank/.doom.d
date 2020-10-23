172f403e3a7e792fd03c0702ba96a1fd06daa293
Asuna <chuxubank@qq.com>
2020-10-23 19:31:17 +0800

    Bump chinese module

--- modules/input/chinese+/packages.el
+++ modules/input/chinese+/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; input/chinese+/packages.el
 
-(package! posframe :pin "7b92a54e588889a74d36d51167e067676db7be8a")
-(package! rime :pin "9d139b56b45e91b787031f5b86c3462287d6ef29")
+(package! posframe :pin "a99da9f40fa864910fd0234bb9e1b6fa52e699c3")
+(package! rime :pin "d2e2f65bea672273cbcbce4a9161264c383a41c2")
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
 (package! valign :recipe (:host github :repo "casouri/valign"))
