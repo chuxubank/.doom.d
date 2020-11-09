d0867f2aac7bf87aed7212d65be0f5bcd10a0ef1
chuxubank <jchu@i-zhiyan.com>
2020-11-09 13:44:24 +0800

    Change org open file to more general fix for wsl

--- +wsl.el
+++ +wsl.el
@@ -4,9 +4,14 @@
 (defconst IS-WSL2 (string-match "-microsoft" operating-system-release))
 (defconst IS-WSL (or IS-WSL1 IS-WSL2))
 
-(defun wsl-open (file)
-  (shell-command (format "%s '%s'" "wsl-open" file)))
+;; https://vxlabs.com/2020/03/07/patch-emacs-org-open-file-using-advice/
+;; fix org-open-file for wsl by temporarily replacing start-process-shell-command with call-process-shell-command
+;; if we don't do this, emacs on WSL will block forever trying to open exported file with windows handler
+(defun wsl-fix-org-open-file (orig-org-open-file &rest args)
+  ;; temporarily replace function,
+  ;; see https://endlessparentheses.com/understanding-letf-and-how-it-replaces-flet.html
+  (cl-letf (((symbol-function 'start-process-shell-command) #'call-process-shell-command))
+    (apply orig-org-open-file args)))
 
 (when IS-WSL
-  (after! org
-    (add-to-list 'org-file-apps '("\\.x?html?\\'" . (lambda (file link) (wsl-open file))))))
+  (advice-add #'org-open-file :around #'wsl-fix-org-open-file))
