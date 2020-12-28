f39f77d4d7d697d0ef9cb63400a59d0bd11009ed
Misaka <chuxubank@qq.com>
2020-12-28 18:06:27 +0800

    Use org-roam-server

--- modules/study/review/config.el
+++ modules/study/review/config.el
@@ -15,3 +15,12 @@
   :after org
   :custom
   (anki-editor-latex-style 'mathjax))
+
+(use-package! org-roam-server
+   :after org-roam
+   :config
+   (defun org-roam-server-open()
+     "Open org-roam server in browser."
+     (interactive)
+     (browse-url-default-browser (format "http://localhost:%d" org-roam-server-port)))
+   (org-roam-server-mode 1))
