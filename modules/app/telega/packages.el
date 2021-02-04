95d24bd6e4128bdd0c3d8a02edc67826c55241d5
Asuna <chuxubank@qq.com>
2021-02-04 23:11:46 +0800

    Pin telega to 1.7.0 for macOS usage

--- modules/app/telega/packages.el
+++ modules/app/telega/packages.el
@@ -1,29 +1,23 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/telega/packages.el
 
-(package! telega)
+(package! telega :pin "f87858583ca7c0e4328f3373aa57d715fd25bbb7")
 
 (when (featurep! +url-shorten)
   (package! telega-url-shorten
-    :recipe
-    (:host github
-     :repo "zevlg/telega.el"
-     :branch "master"
-     :files ("contrib/telega-url-shorten.el"))))
+    :recipe (:type built-in
+             :local-repo "telega.el/contrib"
+             :files ("telega-url-shorten.el"))))
 
 (when (featurep! +alert)
   (package! telega-alert
-    :recipe
-    (:host github
-     :repo "zevlg/telega.el"
-     :branch "master"
-     :files ("contrib/telega-alert.el"))))
+    :recipe (:type built-in
+             :local-repo "telega.el/contrib"
+             :files ("telega-alert.el"))))
 
 (when (featurep! +mnz)
   (package! language-detection)
   (package! telega-mnz
-    :recipe
-    (:host github
-     :repo "zevlg/telega.el"
-     :branch "master"
-     :files ("contrib/telega-mnz.el"))))
+    :recipe (:type built-in
+             :local-repo "telega.el/contrib"
+             :files ("telega-alert.el"))))
